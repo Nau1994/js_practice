9d340705431bc74097413f6e6bb276f0929c7f18
Nau1994 <95983390+Nau1994@users.noreply.github.com>
2022-05-09 23:12:48 +0000

    forLoop

--- forLoop.js
+++ forLoop.js
@@ -8,6 +8,23 @@ function findNeemo(array){
   }
 }
 
+const findNeemo2 = array =>{
+  array.forEach(fish => {
+    if (fish === 'neemo'){
+      console.log('found Neemo !');
+    }
+  })
+}
+
+const findNeemo3 = array =>{
+  for (let fish of array ){
+    if (fish === 'neemo'){
+      console.log('found Neemo !');
+    }
+  }
+}
 
 
 findNeemo(nemo);
+findNeemo2(nemo);
+findNeemo3(nemo);
