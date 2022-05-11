e7a1e9e30cf8daca701b54a28633bb0c84ad048a
NaushadAnsari <95983390+Nau1994@users.noreply.github.com>
2022-05-11 13:52:33 +0000

    charCodeAt

---
+++ charCodeAt.js
@@ -0,0 +1,7 @@
+var ch = 'A';
+var index = 0;
+ 
+var i = ch.charCodeAt(index);
+console.log(i);
+
+console.log(String.fromCharCode(i)); 
---
+++ index.js
@@ -0,0 +1,7 @@
+str='my name is naushad';
+num='m';
+
+ 
+console.log(typeof(str));
+console.log((str.split('')));
+console.log(typeof(Array.from(str)));
