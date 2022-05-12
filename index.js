3bc231dedcebcd5c0c01ac9459fbe85bf4c03f62
NaushadAnsari <95983390+Nau1994@users.noreply.github.com>
2022-05-12 22:20:41 +0000

    Node

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
+class Node{
+  constructor(value){
+    this.value=value;
+    this.next=null;
+  }
+}
+
 class MylinkedList{
 constructor(value){
-  this.head={
-    value:value,
-    next:null  
-  };
+  this.head=new Node(value);
   this.tail=this.head;
   this.length=1;
 }
   append(value){
     //create node
-    let node={
-    value:value,
-    next:null  
-    };
-    let NextNode = this.head;
-    while(1){
-      if (NextNode===this.tail) {
-        NextNode.next=node;
+    let node=new Node(value);
+    this.tail.next = node ;
     this.tail=node;
-    this.length=this.length+1;
-        return ;
-      }
-      NextNode=NextNode.next;
-    }
+     this.length=this.length+1;
+    return this;
+    
+  }
+
+  prepend(value){
+    //create node
+    let node=new Node(value);
+    
+    
+    
+        node.next=this.head;
+        this.head=node;
+        
+        this.length=this.length+1;
+       
+    return this;
     
   }
 
   insert(index,value){
     //create node
-    let node={
-    value:value,
-    next:null  
-    };
-    let NextNode = this.head;
+    let node=new Node(value);
+    let currentNode = this.head;
     if (index===1) {
-        node.next=NextNode;
-        this.head=node;
         
-        this.length=this.length+1;
-        return ;
+        return this.prepend(value) ;
       }
     
     if (index > this.length){ console.log("index couldn't be greater than length"+"("+this.length+")" +" of list"); return ;} 
@@ -46,13 +50,13 @@ constructor(value){
     while(1){
       index=index-1;
       if (index===1) {
-        node.next=NextNode.next;
-        NextNode.next=node;
+        node.next=currentNode.next;
+        currentNode.next=node;
         
         this.length=this.length+1;
         return ;
       }
-      NextNode=NextNode.next;
+      currentNode=currentNode.next;
       
     }
     
@@ -60,47 +64,47 @@ constructor(value){
 
   delete(value){
     
-    let NextNode = this.head;
-    if (NextNode.value===value) {
-        this.head=NextNode.next;
+    let currentNode = this.head;
+    if (currentNode.value===value) {
+        this.head=currentNode.next;
         this.length=this.length-1;
         console.log("Deleted !!");
         return ;
       }
     
     while(1){
-      if (NextNode===this.tail) {
+      if (currentNode===this.tail) {
         console.log("Couldn't find value in list");
       console.log("Not Deleted !!");
         return ;
       }
-      if (NextNode.next.value===value) {
-        if (NextNode.next===this.tail) {
-        this.tail=NextNode;
+      if (currentNode.next.value===value) {
+        if (currentNode.next===this.tail) {
+        this.tail=currentNode;
         }
-        NextNode.next=NextNode.next.next;
+        currentNode.next=currentNode.next.next;
         
         this.length=this.length-1;
         console.log("Deleted !!");
         return ;
       }
-      NextNode=NextNode.next;
+      currentNode=currentNode.next;
       
     }
     
   }
 
   traverse(){
-    let NextNode = this.head;
+    let currentNode = this.head;
     let result = '';
     while(1){
       
-      if (NextNode===this.tail) {
-        result=result+NextNode.value;
+      if (currentNode===this.tail) {
+        result=result+currentNode.value;
         return result;
       }
-      result=result+NextNode.value+'->';
-      NextNode=NextNode.next;
+      result=result+currentNode.value+'->';
+      currentNode=currentNode.next;
     }
   }
   
@@ -115,5 +119,6 @@ mylinkedList.append(19);
 mylinkedList.delete(19);
 mylinkedList.delete(13);
 mylinkedList.delete(11);
+mylinkedList.prepend(23);
 console.log(mylinkedList.traverse(),mylinkedList.length);
 console.log(mylinkedList);
